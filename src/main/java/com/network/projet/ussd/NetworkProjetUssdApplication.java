74bf001f3d684538f3cb6e4e56ee5597c2f8662b
Isa-christ <isabellemagne34@gmail.com>
2025-12-03 17:15:19 +0100

    Initialize network project about USSD integration in web or mobile application

---
+++ src/main/java/com/network/projet/ussd/NetworkProjetUssdApplication.java
@@ -0,0 +1,13 @@
+package com.network.projet.ussd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkProjetUssdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkProjetUssdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/network/projet/ussd/NetworkProjetUssdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.network.projet.ussd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkProjetUssdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
